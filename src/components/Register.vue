215e1e0e8195f9b1f950fbb9c4c7dc2f2060f56b
gabikiwi <gabriel.chivoiu@gmail.com>
2017-11-15 12:13:34 +0200

    adding vuetify to Register and main

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="register">
-    <h1>Register</h1>
-    <input type="email" name="email" v-model="email" placeholder="email"></input>
-    <input type="password" v-model="password" name="password" placeholder="password"></input>
-    <br>
-    <div class="error" v-html="error" />
-    <button @click="register">Register</button>
-  </div>
+  <v-layout coloumn>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input type="email" name="email" v-model="email" placeholder="email"></input>
+          <input type="password" v-model="password" name="password" placeholder="password"></input>
+          <br>
+          <div class="error" v-html="error" />
+          <v-btn class="cyan" @click="register">Register</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -51,6 +59,6 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .error {
-    color:red;
+    color: red;
   }
 </style>
